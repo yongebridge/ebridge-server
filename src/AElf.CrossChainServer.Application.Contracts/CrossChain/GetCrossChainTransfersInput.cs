8693b37732cfe6f8e718ce138976f0cfed5cffe9
boo-ebridge <boo@ebridge.exchange>
2023-06-01 15:17:04 +0800

    fix: add security limit

--- src/AElf.CrossChainServer.Application.Contracts/CrossChain/GetCrossChainTransfersInput.cs
+++ src/AElf.CrossChainServer.Application.Contracts/CrossChain/GetCrossChainTransfersInput.cs
@@ -1,12 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 using Volo.Abp.Application.Dtos;
 
 namespace AElf.CrossChainServer.CrossChain;
 
 public class GetCrossChainTransfersInput : PagedAndSortedResultRequestDto
 {
+    [MaxLength(length:20)]
     public string FromChainId { get; set; }
+    [MaxLength(length:20)]
     public string ToChainId { get; set; }
+    [MaxLength(length:30)]
     public string FromAddress { get; set; }
+    [MaxLength(length:30)]
     public string ToAddress { get; set; }
     public CrossChainStatus? Status { get; set; }
     public CrossChainType? Type { get; set; }
