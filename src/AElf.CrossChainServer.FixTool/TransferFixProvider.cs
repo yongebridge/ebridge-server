e366127d8dbf8a925c209c57d0a1b147217d80e9
sean <sean@ebridge.exchange>
2023-07-20 14:37:41 +0800

    feat: add fix data

--- src/AElf.CrossChainServer.FixTool/TransferFixProvider.cs
+++ src/AElf.CrossChainServer.FixTool/TransferFixProvider.cs
@@ -24,6 +24,7 @@ public class TransferFixProvider
         await Fix_04bd5f6f74666492b507b195c89dccef5187d98b0d917a40555a869011641a13_Async();
         await Fix_954021802ce3d7ccfd7a878c4b7ce73b8b780118e2cebd9691eaa9c5db0ed4b5_Async();
         await Fix_e68602283c49956db567962ccdf585f5fb88fae1911dac4b98652f51f31af254_Async();
+        await Fix_d892be2336f5737ed5765533bbba1e07d10223778782f32d35d794aad09e1b16_Async();
     }
     
     public async Task Fix_04bd5f6f74666492b507b195c89dccef5187d98b0d917a40555a869011641a13_Async()
@@ -147,6 +148,51 @@ public class TransferFixProvider
             Token = reportToken,
             TargetChainId = toChainId,
             LastUpdateHeight = 158999136
+        });
+    }
+    
+    public async Task Fix_d892be2336f5737ed5765533bbba1e07d10223778782f32d35d794aad09e1b16_Async()
+    {
+        var fromChainId = "MainChain_AELF";
+        var toChainId = "BSC";
+        var fromAddress = "L3imsa38dskN2dbDngEpTPbsWopQNqVrk6snpH8RojPTGyS2D";
+        var toAddress = "0xa1d69B7db19e75DD88473BBb99c904901c6d2fE4";
+        var receiptId = "fc958f44a28a4abe387f9be1ebec01c4fd52a457c0397d621b2d8648783a1e08.15";
+        var receiptHash = "6d0acdca16c4245cf0bd7ee367b842527efe70fa300f1000998263c5ef4f4e7b";
+        var reportToken = "0xE6d0B7B043217Da485BE30c57D36E02FDfa7cfc6";
+        var roundId = 48;
+        var transferTransactionId = "d892be2336f5737ed5765533bbba1e07d10223778782f32d35d794aad09e1b16";
+        var amount = 15;
+        
+        var token = await _tokenAppService.GetAsync(new GetTokenInput
+        {
+            Symbol = "ELF",
+            ChainId = fromChainId
+        });
+
+        await _crossChainTransferAppService.TransferAsync(new CrossChainTransferInput
+        {
+            TransferAmount = amount,
+            FromAddress = fromAddress,
+            ToAddress = toAddress,
+            TransferTokenId = token.Id,
+            FromChainId = fromChainId,
+            ToChainId = toChainId,
+            TransferBlockHeight = 159130222,
+            TransferTime = DateTime.Parse("2023-07-09T14:42:52.6799688Z"),
+            TransferTransactionId = transferTransactionId,
+            ReceiptId = receiptId
+        });
+
+        await _reportInfoAppService.CreateAsync(new CreateReportInfoInput
+        {
+            ChainId = fromChainId,
+            ReceiptId = receiptId,
+            ReceiptHash = receiptHash,
+            RoundId = roundId,
+            Token = reportToken,
+            TargetChainId = toChainId,
+            LastUpdateHeight = 159130222
         });
     }
 }
